c85848a1479becc1b64808d17345f1085d2e5840
DenisBardashevich <ackapbinka@mail.ru>
2024-12-13 17:37:05 +0300

    добавил футер

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,51 @@
+import "./Footer.css";
+import FooterConfig from "../../config/footer-config.json";
+import { FaInstagram } from "react-icons/fa6";
+import { GrMailOption } from "react-icons/gr";
+
+const Footer = () => {
+    // Вывод данных из config_____________________________________
+
+    const footerLeft = FooterConfig["footer-left"];
+    const footerRigth = FooterConfig["footer-rigth"];
+    const socialPaths = FooterConfig["footer-socials"];
+    const iconMap = {
+        instagram: <FaInstagram />,
+        email: <GrMailOption />,
+    };
+
+    return (
+        <div className="footer">
+            <div className="footer-content">
+                <div className="footer-left">
+                    <ul>
+                        {footerLeft.map((item) => (
+                            <li key={item.id}>
+                                <strong>{item.name}:</strong> {item.value}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                <div className="footer-rigth">
+                    <ul>
+                        {footerRigth.map((item) => (
+                            <li key={item.id}><span className="cursor-pointer">{item.name}</span></li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+
+            <div className="footer-socials">
+                <ul>
+                    {socialPaths.map((social, index) => (
+                        <li key={index}>
+                            <a href={social.path}>{iconMap[social.name]}</a>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+};
+export default Footer;
