a71d106fa12a407bd35927722dbc1c65267a7377
NausAlexus <naus.lexus@mail.ru>
2024-12-16 00:08:21 +0300

    Начал делать услуги

--- src/components/Main/Services/Services.jsx
+++ src/components/Main/Services/Services.jsx
@@ -1,10 +1,28 @@
 import './Services.css';
+import ServicesData from "../../../config/services-config.json";
 
 function Services() {
+
+	// Данные________________________________
+	const servicesItemsData = ServicesData['services-items'];
+
+
 	return (
-		<>
-            <div>sdjhbshfh</div>
-		</>
+		<div className='services-container'>
+
+			<div className='services-items'>
+				<ul className='services-grid'>
+					{servicesItemsData.map((item) => (
+						<li className='services-item' style={{backgroundImage: `url("${item.img}")`}}>
+							<a className='services-link' href="#">{item.title}</a>
+						</li>
+					))}
+				</ul>
+			</div>
+
+			<div className='services-reviews'></div>
+
+		</div>
 	);
 };
 
