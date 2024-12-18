beb49b4958975e622fc2eafbd17cb15dcbde774c
NausAlexus <naus.lexus@mail.ru>
2024-12-18 13:59:00 +0300

    Закончил item

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,11 +1,17 @@
-import React, { Suspense, lazy } from 'react';
+import React, { lazy } from 'react';
    const Home = lazy(() => import('./Home/Home'));
    const Services = lazy(() => import('./Services/Services'));
    const GiftSertificat = lazy(() => import('./GiftCertificat/GiftCertificat'));
+   const Item = lazy(() => import('./Item/Item'));
 import { Routes, Route } from 'react-router-dom';
 import ScrollToTop from '../../ScrollToTop';
+import ItemConfig from "../../config/item-config.json";
 
 function Main() {
+
+	// Данные
+	const ItemData = ItemConfig.item;
+
 	return (
 		<>
 			<ScrollToTop />
@@ -13,6 +19,17 @@ function Main() {
 				<Route path="/" element={<Home />} />
 				<Route path="/services" element={<Services />} />
 				<Route path="/setificat" element={<GiftSertificat />} />
+                {ItemData.map((item) => (
+                    <Route 
+                        key={item.id} 
+                        path={item.path} 
+                        element={<Item 
+                            titleItem={item.title}
+                            descriptionItem={item.description}
+                            serviceItem={item.services}
+                        />} 
+                    />
+                ))}
 			</Routes>
 		</>
 	);
