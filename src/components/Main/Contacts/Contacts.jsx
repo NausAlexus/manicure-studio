882fdb35dc28396df38a10a3ea6a768415646a2b
NausAlexus <naus.lexus@mail.ru>
2024-12-18 14:08:38 +0300

    Оптимизация карты

--- src/components/Main/Contacts/Contacts.jsx
+++ src/components/Main/Contacts/Contacts.jsx
@@ -1,5 +1,4 @@
 import "./Contacts.css";
-import imgMap from "./../../../../public/Screenshot_1.png";
 import ContactsConfig from "./../../../config/contacts-config.json";
 
 const Contacts = () => {
@@ -27,7 +26,7 @@ const Contacts = () => {
                         </div>
                     </div>
                     <div className="contacts-container-rigth">
-                        <a target="_blank" href="https://www.google.ru/maps/place/%D0%9C%D0%B8%D0%BD%D1%81%D0%BA,+%D0%9C%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C/@53.8923173,27.5001467,12z/data=!4m6!3m5!1s0x46dbcfd35b1e6ad3:0xb61b853ddb570d9!8m2!3d53.9006011!4d27.558972!16zL20vMGRseGo?hl=ru&entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"><img src={imgMap} alt="Map" /></a>
+                        <a target="_blank" href="https://www.google.ru/maps/place/%D0%9C%D0%B8%D0%BD%D1%81%D0%BA,+%D0%9C%D0%B8%D0%BD%D1%81%D0%BA%D0%B0%D1%8F+%D0%BE%D0%B1%D0%BB%D0%B0%D1%81%D1%82%D1%8C/@53.8923173,27.5001467,12z/data=!4m6!3m5!1s0x46dbcfd35b1e6ad3:0xb61b853ddb570d9!8m2!3d53.9006011!4d27.558972!16zL20vMGRseGo?hl=ru&entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"><img src="https://i.ibb.co/BGMvrFX/Screenshot-1.webp" alt="Map" /></a>
                     </div>
                 </div>
             </div>
