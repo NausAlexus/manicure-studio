0b1e1e1eb4413301c009cd7992d3405d081651aa
NausAlexus <naus.lexus@mail.ru>
2024-12-30 14:23:53 +0300

    Делаем выбор специалистов

--- src/components/Reservation/MasterSelect/MasterSelect.jsx
+++ src/components/Reservation/MasterSelect/MasterSelect.jsx
@@ -1,11 +1,26 @@
 import './MasterSelect.css';
+import serviceData from '../../../config/reservation-config.json'
 
 function MasterSelect() {
 
+	const serviceMasterData = serviceData.manicure;
+
 	return (
-		<>
-            Выбор специалиста
-		</>
+		<div className='master-select'>
+            <h3 className='master-select-title'>Выбор специалиста:</h3>
+            <ul className='master-select-items'>
+				{serviceMasterData.map(el => (
+					<li className='master-select-item' key={el.id}>
+						<img className='master-select-img' src={el.avatar} alt="Master" />
+						<div>
+							<h3>{el.name}</h3>
+							<p>{el.role}</p>
+							<p >⭐{el.rating.toFixed(1)}</p>
+						</div>
+					</li>
+				))}
+            </ul>
+		</div>
 	);
 };
 
