35455767c8558f246f969888bcbca7bc36303501
NausAlexus <naus.lexus@mail.ru>
2025-01-04 12:40:02 +0300

    Реализовал меню

--- src/components/Reservation/Menu/Menu.jsx
+++ src/components/Reservation/Menu/Menu.jsx
@@ -2,54 +2,30 @@ import './Menu.css';
 import ServiceSelect from '../ServiceSelect/ServiceSelect';
 import MasterSelect from '../MasterSelect/MasterSelect';
 import DataTimeSelect from '../DataTimeSelect/DataTimeSelect';
-import { useState } from 'react';
 import serviceData from '../../../config/reservation-config.json';
 
 function Menu(props) {
-    const serviceManicureData = serviceData.manicure;
-    const servicePedicureData = serviceData.pedicure;
-    const serviceMakeupData = serviceData.makeup;
-    const serviceHairdressersData = serviceData.hairdressers;
-    const serviceCosmetologistsData = serviceData.cosmetologists;
-    const serviceLeshmakerData = serviceData.leshmaker;
-
-    const [selectedService, setSelectedService] = useState(null);
-    const [selectedMaster, setSelectedMaster] = useState(null); // Состояние для хранения выбранного мастера
-
-    const handleServiceSelect = (service) => {
-        setSelectedService(service);
-        setSelectedMaster(null); // Сброс выбранного мастера при выборе службы
-        // Вызов функции из props
-        props.handleServiceSelect(service);
-    };
-
-    const handleMasterSelect = (master) => {
-        setSelectedMaster(master); // Установка выбранного мастера
-        // Вызов функции из props
-        props.handleMasterSelect(master.name);
-    };
-
-    const mastersData = selectedService === 'маникюр' ? serviceManicureData :
-        selectedService === 'педикюр' ? servicePedicureData :
-        selectedService === 'макияж' ? serviceMakeupData :
-        selectedService === 'парикмахерские услуги' ? serviceHairdressersData :
-        selectedService === 'косметические услуги' ? serviceCosmetologistsData :
-        selectedService === 'наращивание ресниц' ? serviceLeshmakerData : [];
+    const mastersData = props.selectedService === 'маникюр' ? serviceData.manicure :
+        props.selectedService === 'педикюр' ? serviceData.pedicure :
+        props.selectedService === 'макияж' ? serviceData.makeup :
+        props.selectedService === 'парикмахерские услуги' ? serviceData.hairdressers :
+        props.selectedService === 'косметические услуги' ? serviceData.cosmetologists :
+        props.selectedService === 'наращивание ресниц' ? serviceData.leshmaker : [];
 
     return (
         <div className='menu-container'>
-            {selectedMaster === null ? (
-                selectedService === null ? (
-                    <ServiceSelect 
-                        onServiceSelect={handleServiceSelect} 
-                    />
-                ) : (
-                    <MasterSelect 
-                        mastersData={mastersData} 
-                        onMasterSelect={handleMasterSelect} 
-                    />
-                )
-            ) : (
+            {props.currentComponent === 'serviceSelect' && (
+                <ServiceSelect 
+                    onServiceSelect={props.handleServiceSelect} 
+                />
+            )}
+            {props.currentComponent === 'masterSelect' && (
+                <MasterSelect 
+                    mastersData={mastersData} 
+                    onMasterSelect={props.handleMasterSelect} 
+                />
+            )}
+            {props.currentComponent === 'dataTimeSelect' && (
                 <DataTimeSelect 
                     userData={props.userData} 
                     disvisibleClick={props.disvisibleClick} 
--- src/components/Reservation/Reservation.jsx
+++ src/components/Reservation/Reservation.jsx
@@ -1,20 +1,59 @@
 import './Reservation.css';
 import { IoCloseSharp } from "react-icons/io5";
+import { MdOutlineAddLink } from "react-icons/md";
+import { IoMdPeople } from "react-icons/io";
+import { HiMiniCalendar } from "react-icons/hi2";
 import PropTypes from 'prop-types';
 import Menu from './Menu/Menu';
+import { useState } from 'react'; // Импортируем useState
 
 function Reservation(props) {
+    const [selectedService, setSelectedService] = useState(null); // Состояние для хранения выбранного сервиса
+    const [selectedMaster, setSelectedMaster] = useState(null); // Состояние для хранения выбранного мастера
+    const [currentComponent, setCurrentComponent] = useState('serviceSelect'); // Новый статус для текущего компонента
 
-	return (
+    const handleClose = () => {
+        props.disvisibleClick(); // Закрывает модальное окно
+        setSelectedService(null); // Сбрасываем выбранный сервис
+        setSelectedMaster(null); // Сбрасываем выбранного мастера
+        setCurrentComponent('serviceSelect'); // Сбрасываем текущий компонент на ServiceSelect при закрытии
+    };
+
+    const switchComponent = (component) => {
+        setCurrentComponent(component); // Меняем текущий компонент
+    };
+
+    // Обработка выбора сервиса
+    const handleServiceSelect = (service) => {
+        setSelectedService(service);
+        props.handleServiceSelect(service);
+        setCurrentComponent('masterSelect'); // Переходим к выбору мастера
+    };
+
+    // Обработка выбора мастера
+    const handleMasterSelect = (master) => {
+        setSelectedMaster(master);
+        props.handleMasterSelect(master.name);
+        setCurrentComponent('dataTimeSelect'); // Переходим к выбору даты и времени
+    };
+
+    return (
         <>
             <div onClick={props.disvisibleClick} className='reservation-background' style={{ display: props.visible ? "block" : "none"}}></div>
             <div className='reservation-container' style={{ transform: `translate(-50%, -50%) scale(${props.visible ? 1 : 0})` }}>
-                <IoCloseSharp className='reservation-close-btn' onClick={props.disvisibleClick}/>
+                <div className='reservation-menu-block'>
+                    <div>
+                        <MdOutlineAddLink className='reservation-menu-block-btn' onClick={() => switchComponent('serviceSelect')} />
+                        <IoMdPeople className='reservation-menu-block-btn' onClick={() => switchComponent('masterSelect')} />
+                        <HiMiniCalendar className='reservation-menu-block-btn' onClick={() => switchComponent('dataTimeSelect')} />
+                    </div>
+                    <IoCloseSharp className='reservation-close-btn' onClick={handleClose} />
+                </div>
                 <h3 className='reservation-title'>Запись на приём</h3>
                 <div className="reservation-content">
                     <Menu 
-                        handleServiceSelect={props.handleServiceSelect}
-                        handleMasterSelect={props.handleMasterSelect}
+                        handleServiceSelect={handleServiceSelect}
+                        handleMasterSelect={handleMasterSelect}
                         disvisibleClick={props.disvisibleClick}
                         handleDateChange={props.handleDateChange}
                         handleTimeChange={props.handleTimeChange}
@@ -22,15 +61,27 @@ function Reservation(props) {
                         handleNameChange={props.handleNameChange}
                         handlePhoneChange={props.handlePhoneChange}
                         userData={props.userData}
+                        selectedService={selectedService} // Передаем выбранный сервис
+                        selectedMaster={selectedMaster} // Передаем выбранного мастера
+                        currentComponent={currentComponent} // Передаем текущий компонент
                     />
                 </div>
             </div>
         </>
-	);
+    );
 };
+
 Reservation.propTypes = {
     disvisibleClick: PropTypes.func.isRequired, // Функция для закрытия
     visible: PropTypes.bool.isRequired, // Булевое значение для видимости
+    handleServiceSelect: PropTypes.func.isRequired,
+    handleMasterSelect: PropTypes.func.isRequired,
+    handleDateChange: PropTypes.func,
+    handleTimeChange: PropTypes.func,
+    handleSubmit: PropTypes.func,
+    handleNameChange: PropTypes.func,
+    handlePhoneChange: PropTypes.func,
+    userData: PropTypes.object,
 };
 
 export default Reservation;
